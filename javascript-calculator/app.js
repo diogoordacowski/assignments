12fc7c215688e3fd69d6b63349d65c27fdb58e70
Diogo Ordacowski <diogoordacowski@gmail.com>
2021-02-08 20:35:39 -0700

    updated calculator js to be dry

--- javascript-calculator/app.js
+++ javascript-calculator/app.js
@@ -1,27 +1,21 @@
 const readline = require("readline-sync")
 
-const num1 = parseFloat(readline.question("Please enter your first number: "))
-const num2 = parseFloat(readline.question("Please enter your second number: "))
-const operation = readline.question("Please enter the operation to perform: add, sub, mul, div ")
+var num1 = Number(readline.question("Please enter your first number: "))
+var num2 = Number(readline.question("Please enter your second number: "))
+var operation = readline.question("Please enter the operation to perform: add, sub, mul, div ")
 
 var result
 
 if (operation === "add") {
     result = num1 + num2
-    console.log("The result is " + result)
-} else if (operation === "sub") {
-    result = num1 - num2
-    console.log("The result is " + result)
-} else if (operation === mul) {
-    result = num1 * num2
-    console.log("The result is " + result)
-} else if (operation === div) {
-    result = num1 / num2
-    console.log("The result is " + result)
-} else {
-    console.log("This failed because of a bad input")
-}
+    } else if (operation === "sub") {
+        result = num1 - num2
+    } else if (operation === "mul") {
+        result = num1 * num2
+    } else if (operation === "div") {
+        result = num1 / num2
+    } else {
+        result = "not possible because I don't know what operation to use"
+    }
 
-
-
-
+console.log("The result is " + result)
