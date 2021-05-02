9651849ce5aa7b8ec870235673b3af03251a7714
Diogo Ordacowski <diogoordacowski@gmail.com>
2021-05-02 10:58:44 -0600

    blog props improved

--- blop-props/src/Header.js
+++ blop-props/src/Header.js
@@ -4,18 +4,21 @@ import React from "react"
 function Header(props) {
 
     return (
+        
         <div className="header-container">
+            <nav>
+                <a href="https://www.google.com">Start Bootstrap</a>
+                <button className="nav-menu-btn">Menu ☰</button>
+            </nav>
+            <div className="background"></div>
             <div className="overlay"></div>
-                <nav>
-                    <a href="https://www.google.com">Start Bootstrap</a>
-                    <button className="nav-menu-btn">Menu ☰</button>
-                </nav>
-                <div className="header-title">
-                    
-                    <h1>Clean Blog</h1>
-                    <h2>A Blog Theme by Start Bootstrap </h2>
-                </div>
+            <div className="header-title">
+                <h1>Clean Blog</h1>
+                <h2>A Blog Theme by Start Bootstrap </h2>
+            </div>
+
         </div>
+ 
     )
 }
 
