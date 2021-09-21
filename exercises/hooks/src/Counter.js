c1c506d0432688d2345ac0f826a41ed2d71a5bad
Diogo Ordacowski <diogoordacowski@gmail.com>
2021-09-21 01:15:18 -0600

    AutoCounter introduced, but needs to be converted to component

--- exercises/hooks/src/Counter.js
+++ exercises/hooks/src/Counter.js
@@ -5,6 +5,8 @@ function Counter() {
 
     const [ count, setCount ] = useState(0)
     const [ color, setColor ] = useState("")
+    const [autoCount, setAutoCount ] = useState(0)
+    const [displayAutoCount, setDisplayAutoCount ] = useState(true)
 
     function increment() {
         setCount(prevCount => prevCount + 1)
@@ -15,15 +17,38 @@ function Counter() {
 
     useEffect(() => {
         setColor(randomColor())
-    }, [count])
+    }, [count, autoCount])
+
+    useEffect(() => {
+        
+        const intervalId = setInterval(() => {
+            setAutoCount(prevAutoCount => prevAutoCount + 1)
+            console.log("we are counting every second")
+        }, 1000)
+
+        return () => {
+            clearInterval(intervalId)
+            console.log("the component unmounted and this interval ended")
+        }
+
+    }, [])
 
     return (
         <div>
             <hr></hr>
             <p>The counter below is using useState to handle the change in number</p>
             <h1 style={{color: color}}>{count}</h1>
-            <button onClick = {increment}>Add number!</button>
-            <button onClick = {decrement}>Lower number!</button>
+            <button onClick = {increment}>Add number</button>
+            <button onClick = {decrement}>Lower number</button>
+            <br />
+            <h1>Auto Counter</h1>
+            <button onClick={
+                () => {setDisplayAutoCount(prevDisplayAutoCount => !prevDisplayAutoCount)}
+            }>
+                {displayAutoCount ? "Unmount AutoCounter" : "Mount AutoCounter" }</button>
+            {
+                displayAutoCount ? <h2 style={{color:color}}>{autoCount}</h2> : null
+            }
         </div>
 
     )
