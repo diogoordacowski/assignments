2f57516c3b8bbcdd924f5ec341a40dfd89858ec4
Diogo Ordacowski <diogoordacowski@gmail.com>
2021-09-23 23:37:11 -0600

    game ui complete

---
+++ exercises/speed-typing-game/src/App.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+function App() {
+
+    return (
+        <div class="gamebox">
+            <h1>Speed Typing Game!</h1>
+            <textarea />
+            <h4>Remaining Time Goes Here</h4>
+            <div class="buttonbox">
+                <button>Start Game! </button>
+            </div>
+            <h1>Score:</h1>
+
+        </div>
+    )
+
+}
+
+export default App
