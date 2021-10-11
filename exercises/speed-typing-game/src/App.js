4fe3c379f7d4a6f662a6e7965cabaa00bfd19d3b
Diogo Ordacowski <diogoordacowski@gmail.com>
2021-10-10 22:22:15 -0600

    theme context working for button

--- exercises/speed-typing-game/src/App.js
+++ exercises/speed-typing-game/src/App.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect, useRef } from "react"
+import React, { useState, useEffect, useRef, useContext } from "react"
+import { ThemeContext } from "./themeContext"
 
-function App() {
+function App(props) {
 
     const START_TIME = 15
 
@@ -9,6 +10,7 @@ function App() {
     const [isTimeRunning, setisTimeRunning] = useState(false)
     const [answer, setAnswer] = useState("tbd")
     const textareaRef = useRef(null)
+    const context = useContext(ThemeContext)
 
     function handleChange(e) {
         const { value } = e.target
@@ -43,37 +45,46 @@ function App() {
     function toggleTimer() {
         if (isTimeRunning === false && timeRemaining > 0) {
             setisTimeRunning(true)
+            console.log(context.theme)
 
         } else if (isTimeRunning === false && timeRemaining === 0) {
             setTimeRemaining(START_TIME)
             setText("")
             setisTimeRunning(true)
-            
+
         }
     }
 
     return (
-        <div className="gamebox">
-            <h1>Speed Typing Game!</h1>
-            <textarea
-                ref={textareaRef}
-                onChange={handleChange}
-                name="text"
-                value={text}
-                disabled={!isTimeRunning}
+        
+
+
+            <div className="gamebox" >
+                <h1>Speed Typing Game!</h1>
+                <div className="theme-button">
+                    <button className={`${context.theme}-theme`} onClick={context.toggleTheme} >Change Theme</button>
+                </div>
+                <textarea
+                    ref={textareaRef}
+                    onChange={handleChange}
+                    name="text"
+                    value={text}
+                    disabled={!isTimeRunning}
                 />
-            <h4>Time Remaining: {timeRemaining} </h4>
-            <div className="buttonbox">
-                {isTimeRunning ? null : <button onClick={toggleTimer}>
+                <h4>Time Remaining: {timeRemaining} </h4>
+                <div className="buttonbox">
+                    {isTimeRunning ? null : <button onClick={toggleTimer} className={`${context.theme}-theme`} >
 
-                    {(isTimeRunning === false && timeRemaining === 0) ? "Play Again!" : "Start Game!"}
+                        {(isTimeRunning === false && timeRemaining === 0) ? "Play Again!" : "Start Game!"}
 
-                </button>}
+                    </button>}
+
+                </div>
+                <h1>Score: {answer} </h1>
 
             </div>
-            <h1>Score: {answer} </h1>
 
-        </div>
+    
     )
 
 }
