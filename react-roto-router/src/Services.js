5bd51e66ebf2bd0097c03fc4066a8356542ff5c1
Diogo Ordacowski <diogoordacowski@gmail.com>
2021-10-17 12:50:53 -0600

    added data object and use them in services list page

--- react-roto-router/src/Services.js
+++ react-roto-router/src/Services.js
@@ -1,8 +1,16 @@
 import React from "react"
+import { Link, Switch, Route } from "react-router-dom"
+import servicesObject from "./servicesObject"
 
 function Services() {
-    return(
-        <h1>Services Page Is Loaded</h1>
+
+    const servicesArr = servicesObject.map(service => (<h3>{service.name} - ${service.price}</h3>))
+
+    return (
+        <div>
+            <h1>Services Page Is Loaded</h1>
+            {servicesArr}
+        </div>
     )
 }
 
