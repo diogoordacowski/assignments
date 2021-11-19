8977cf62e81e677df894448687bc0972a24ad44c
Diogo Ordacowski <diogoordacowski@gmail.com>
2021-11-18 21:56:14 -0700

    useLocation added for training purposes only

--- react-roto-router/src/Services.js
+++ react-roto-router/src/Services.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link, useRouteMatch } from "react-router-dom"
+import { Link, useRouteMatch, useLocation } from "react-router-dom"
 import servicesObject from "./servicesObject"
 
 function Services() {
@@ -10,7 +10,11 @@ function Services() {
         <h3 key={service._id}><Link to={`${url}/${service._id}`}>{service.name}</Link> - ${service.price}</h3>
         
     ))
-
+    
+    //------- doesn't do anything visible on app, is only included here for training purposes
+    const location = useLocation()
+    console.log(location)
+    // --------
     return (
         <div>
             <h1>Services Page Is Loaded</h1>
